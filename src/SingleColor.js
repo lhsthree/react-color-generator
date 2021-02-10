9c349c611c4e8812c36dd5db85d9e72734b728a1
lhsthree <lhsthree@gmail.com>
2021-02-10 17:37:00 -0600

    first commit

---
+++ src/SingleColor.js
@@ -0,0 +1,9 @@
+  
+import React, { useState, useEffect } from 'react'
+import rgbToHex from './utils'
+
+const SingleColor = () => {
+  return <h4>single color</h4>
+}
+
+export default SingleColor
