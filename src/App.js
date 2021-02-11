1f708eaff0e783e36c1cffc98d19fa843aaa72b2
lhsthree <lhsthree@gmail.com>
2021-02-11 13:29:30 -0600

    feat: added default value in for page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Values from 'values.js'
 function App() {
 	const [color,setColor] = useState('')
 	const [error,setError] = useState(false)
-	const [list, setList] = useState([])
+	const [list, setList] = useState(new Values("#222").all(10))
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
@@ -29,7 +29,7 @@ function App() {
 	    			type="text" 
 	    			value={color} 
 	    			onChange={(e) => setColor(e.target.value)}
-	    			placeholder="#f15025"
+	    			placeholder="#222222"
 	    			className={error ? 'error' : null}
 	    		/>
 	    		<button 
--- src/SingleColor.js
+++ src/SingleColor.js
@@ -7,6 +7,12 @@ const SingleColor = ({rgb, weight, index, hexColor}) => {
 	const bcg = rgb.join(',')
 	const hex = rgbToHex(...rgb)
 	const hexValue = `#${hexColor}`
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			setAlert(false)
+		}, 1500)
+		return () => clearTimeout(timeout)
+	},[alert])
   return (
   	<article 
     	className={`color ${index > 9 && 'color-light' }`} 
